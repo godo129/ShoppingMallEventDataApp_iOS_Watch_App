edf4dd261e55f730383c477057619cc2f6a09df6
godo <godo129@naver.com>
2021-12-16 22:59:17 +0900

    홈 화면 설정

---
+++ EventDataApp WatchKit Extension/Controller/HomeInterfaceController.swift
@@ -0,0 +1,38 @@
+//
+//  HomeInterfaceController.swift
+//  EventDataApp WatchKit Extension
+//
+//  Created by hong on 2021/12/16.
+//
+
+import WatchKit
+import Foundation
+
+
+class HomeInterfaceController: WKInterfaceController {
+    
+    @IBOutlet var table: WKInterfaceTable!
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        table.setNumberOfRows(ShoppingMallList.count, withRowType: "row")
+        
+        for idx in 0..<ShoppingMallList.count {
+            let row = table.rowController(at: idx) as! RowController
+            row.image.setImage(UIImage(named: ShoppingMallList[idx]))
+            row.label.setText(ShoppingMallList[idx])
+        }
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
