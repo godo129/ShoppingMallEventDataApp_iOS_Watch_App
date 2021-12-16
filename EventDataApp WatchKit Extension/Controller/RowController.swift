cc75b43644d1318f5303af879b60f2ded9140b35
godo <godo129@naver.com>
2021-12-16 22:59:33 +0900

    홈 테이블 설정

---
+++ EventDataApp WatchKit Extension/Controller/RowController.swift
@@ -0,0 +1,16 @@
+//
+//  RowController.swift
+//  EventDataApp WatchKit Extension
+//
+//  Created by hong on 2021/12/16.
+//
+
+import UIKit
+import WatchKit
+
+class RowController: NSObject {
+    
+  @IBOutlet var image: WKInterfaceImage!
+  @IBOutlet var label: WKInterfaceLabel!
+
+}
