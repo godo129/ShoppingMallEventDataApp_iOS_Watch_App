3148862ae5ce1b721c49e66fdd35f6d1e8fcdce6
godo <godo129@naver.com>
2021-12-16 23:40:37 +0900

    이벤트 사진 보이게 하기

---
+++ EventDataApp WatchKit Extension/Controller/EventInterfaceController.swift
@@ -0,0 +1,65 @@
+//
+//  EventInterfaceController.swift
+//  EventDataApp WatchKit Extension
+//
+//  Created by hong on 2021/12/16.
+//
+
+import WatchKit
+import Foundation
+
+
+class EventInterfaceController: WKInterfaceController {
+
+    var eventManger = EventManger()
+    
+    @IBOutlet var table: WKInterfaceTable!
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        eventManger.delegate = self
+        eventManger.getEventData(shoppingMall: selectedShoppingMall)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
+
+extension EventInterfaceController: EventManagerDelegate {
+    func didUpdateEventData(_ eventManger: EventManger, event: EventModel) {
+
+        DispatchQueue.main.async {
+            
+            print(event.imageURL)
+            self.table.setNumberOfRows(event.imageURL.count, withRowType: "Row")
+            
+            for idx in 0..<event.imageURL.count {
+                let row = self.table.rowController(at: idx) as! EventRowController
+                
+                if let url = URL(string: event.imageURL[idx]) {
+                    // Fetch Image Data
+                    if let data = try? Data(contentsOf: url) {
+                        row.image.setImageData(data)
+                    }
+                    
+                }
+   
+            }
+            
+        }
+    }
+    func didFailUpdateEventData(error: Error) {
+        print(error)
+    }
+}
