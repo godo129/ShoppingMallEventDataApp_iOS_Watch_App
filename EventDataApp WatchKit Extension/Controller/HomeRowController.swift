d5d3181191067eaf1cac7eccd5737da26e0dd914
godo <godo129@naver.com>
2021-12-16 23:41:13 +0900

    홈 테이블 설정

---
+++ EventDataApp WatchKit Extension/Controller/HomeRowController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeRowController.swift
+//  EventDataApp WatchKit Extension
+//
+//  Created by hong on 2021/12/16.
+//
+
+import UIKit
+import WatchKit
+
+class HomeRowController: NSObject {
+    
+  @IBOutlet var image: WKInterfaceImage!
+  @IBOutlet var label: WKInterfaceLabel!
+
+}
