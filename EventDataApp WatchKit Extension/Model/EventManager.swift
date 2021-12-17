58723bff21dcd3f879fc338383b6df13b716e72b
godo <godo129@naver.com>
2021-12-17 22:32:42 +0900

    함수 갱신

--- EventDataApp WatchKit Extension/Model/EventManager.swift
+++ EventDataApp WatchKit Extension/Model/EventManager.swift
@@ -14,7 +14,10 @@ protocol EventManagerDelegate {
     func didFailUpdateEventData(error: Error)
 }
 
-
+protocol ShoppingMallDelegate {
+    func didUpdateShopData(_ eventManager: ShoppingMallManager, data: ShoppingModel)
+    func didFailUpdateShopData(error: Error)
+}
 
 struct EventManger {
     
@@ -65,3 +68,57 @@ struct EventManger {
 }
 
 
+struct ShoppingMallManager {
+    
+    var delegate: ShoppingMallDelegate?
+    
+    func getNameList() {
+        
+        let url = "https://gg6hx0.deta.dev/"
+        
+        AF.request(url,method: .get).validate().responseJSON { (response) in
+            switch response.result {
+            case .success(let value):
+                let eventJSON: JSON = JSON(value)
+                if let datas = getLogoData(json: eventJSON) {
+                    self.delegate?.didUpdateShopData(self,data: datas)
+                 
+                }
+            case let .failure(error):
+                
+                self.delegate?.didFailUpdateShopData(error: error)
+
+            }
+        }
+        
+    }
+    
+    func getLogoData(json:JSON) -> ShoppingModel? {
+    
+        
+        guard let list = json["쇼핑몰 리스트"].array else {return nil}
+        
+        let dataList = list.map{$0.stringValue}
+        
+        guard let logoList = json["로고"].dictionary else {return nil}
+        
+        var logoURLList: [String:String] = [:]
+        
+        for item in dataList {
+            
+            guard let logoURL = logoList[item] else {return nil}
+            
+            logoURLList[item] = logoURL.stringValue
+        }
+        
+        let logoDatas = logoData(logo: logoURLList)
+        
+        let shoppingMallLogoData = ShoppingModel(ShopList: dataList, logo: logoDatas)
+    
+        return shoppingMallLogoData
+
+
+    }
+    
+    
+}
